5c201c50df1fbf09ebb40f6a0bda110871cad14f
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2016-07-03 23:57:13 +0530

    Create Register.java

---
+++ app_dev_servlet/simple/form_db_file/Register.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class Register extends HttpServlet {
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+	
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String pass = request.getParameter("pass");
+        try{
+        
+        //loading drivers for mysql
+        Class.forName("com.mysql.jdbc.Driver");
+
+	//creating connection with the database 
+          Connection  con=DriverManager.getConnection
+                     ("jdbc:mysql://localhost:3306/test","root","");
+
+        PreparedStatement ps=con.prepareStatement
+   ("insert into Student values(?,?,?)");
+
+        ps.setString(1, name);
+        ps.setString(2, email);
+        ps.setString(3, pass);
+        int i=ps.executeUpdate();
+        
+          if(i>0)
+          {
+            out.println("You are sucessfully registered");
+          }
+        
+        }
+        catch(Exception se)
+        {
+            se.printStackTrace();
+        }
+	
+      }
+  }
