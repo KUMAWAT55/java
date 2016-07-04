98fbf6609e00dd470820ea59df4bd240657d6d95
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2016-07-04 15:09:52 +0530

    Update RegisterServ.java

--- app_dev_servlet/login_dispatch/RegisterServ.java
+++ app_dev_servlet/login_dispatch/RegisterServ.java
@@ -1,1 +1,96 @@
 
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterServ
+ */
+@WebServlet("/RegisterServ")
+public class RegisterServ extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public RegisterServ() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	    response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+	
+        String name = request.getParameter("userName");
+        //String email = request.getParameter("userEmail");
+        String pass = request.getParameter("userPass");
+        
+        System.out.println(name+"  "+pass); 
+        if(pass.equals("rohit")){
+        	RequestDispatcher rd=request.getRequestDispatcher("servlet2");
+        	rd.forward(request, response);
+        	}
+        else{
+        	out.println("mismatched!!!!!!");
+        	RequestDispatcher rd=request.getRequestDispatcher("/index.html");
+        	rd.include(request, response);
+        }
+        
+      /*  try{
+        
+        //loading drivers for mysql
+        Class.forName("com.mysql.jdbc.Driver");
+
+	//creating connection with the database 
+          Connection  con=DriverManager.getConnection
+                     ("jdbc:mysql://localhost:3306/test","root","");
+
+        PreparedStatement ps=con.prepareStatement
+                  ("insert into student values(?,?,?)");
+
+        ps.setString(1, name);
+        ps.setString(2, email);
+        ps.setString(3, pass);
+        int i=ps.executeUpdate();
+        
+          if(i>0)
+          {
+            out.println("You are sucessfully registered");
+          }
+        
+        }
+        catch(Exception se)
+        {
+            se.printStackTrace();
+        }
+        */
+	
+      }
+
+	}
+
+
