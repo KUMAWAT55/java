61f836eed6fd38b9f4482601ae4f7bf7a90288bf
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2016-07-07 13:56:37 +0530

    Update download.java

--- app_dev_servlet/downloading/download.java
+++ app_dev_servlet/downloading/download.java
@@ -1,1 +1,60 @@
 
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class download
+ */
+@WebServlet("/download")
+public class download extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public download() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		response.setContentType("text/html");  
+		PrintWriter out = response.getWriter();  
+		String filename = "rohit.txt";   
+		String filepath ="f:\\";   
+		response.setContentType("APPLICATION/OCTET-STREAM");   
+		response.setHeader("Content-Disposition","attachment; filename=\"" + filename + "\"");   
+		  
+		FileInputStream fileInputStream = new FileInputStream(filepath + filename);  
+		            
+		int i;   
+		while ((i=fileInputStream.read()) != -1) {  
+		out.write(i);   
+		}   
+		fileInputStream.close();   
+		out.close();   
+		}  
+	}
+
+
+
