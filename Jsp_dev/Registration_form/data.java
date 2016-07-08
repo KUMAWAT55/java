66641c4244dfa67185d490ef67fdb4b812c0e150
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2016-07-08 19:03:33 +0530

    Update data.java

--- Jsp_dev/Registration_form/data.java
+++ Jsp_dev/Registration_form/data.java
@@ -1,1 +1,33 @@
 
+package regis;
+import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class data {
+		
+		
+			
+		public static int response(user u){
+			int s=0;
+			try{
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
+		PreparedStatement stmt=con.prepareStatement("insert into user values(?,?,?)");
+		
+		stmt.setString(1, u.getUname());
+		stmt.setString(2, u.getUemail());
+		stmt.setString(3, u.getUpass());
+		stmt.executeUpdate();
+		System.out.println(u.getUname()+" "+u.getUemail()+" "+u.getUpass());
+		
+		System.out.println("success");
+		
+		}catch(Exception e){
+			System.out.println(e);
+		}
+		
+	return s;}
+		
+	}
+
