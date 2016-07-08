3df305c409a79bb136eae10ee90ae9f31280d03f
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2016-07-08 19:04:13 +0530

    Update user.java

--- Jsp_dev/Registration_form/user.java
+++ Jsp_dev/Registration_form/user.java
@@ -1,1 +1,36 @@
 
+    package regis;
+
+//import javax.xml.ws.Response;
+
+//import org.omg.CORBA.Request;
+
+public class user {  
+    private String uname,upass,uemail;  
+      
+    public String getUname() {  
+        return uname; 
+        
+    }  
+      
+    public void setUname(String uname) {  
+        this.uname = uname;  
+    }  
+      
+    public String getUpass() {  
+        return upass;  
+    }  
+      
+    public void setUpass(String upass) {  
+        this.upass = upass;  
+    }  
+      
+    public String getUemail() {  
+        return uemail;  
+    }  
+      
+    public void setUemail(String uemail) {  
+        this.uemail = uemail;  
+    }  
+      
+    }  
