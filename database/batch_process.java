cd5b3b0bff4aea680a535792e858121f2db4f761
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2016-07-09 22:45:10 +0530

    Update batch_process.java

--- database/batch_process.java
+++ database/batch_process.java
@@ -1,1 +1,14 @@
+import java.sql.*;
 
+import java.util.Scanner;
+
+public class batch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
+			Scanner sc=new Scanner(System.in);
+			PreparedStatement stmt=con.prepareStatement("insert into user values(?,?,?)");
+			while(true){
