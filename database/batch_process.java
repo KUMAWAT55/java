4a21284869aa1ba8bae77acc2745f4407db74e7d
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2016-07-09 22:46:13 +0530

    Update batch_process.java

--- database/batch_process.java
+++ database/batch_process.java
@@ -12,3 +12,13 @@ public class batch {
 			Scanner sc=new Scanner(System.in);
 			PreparedStatement stmt=con.prepareStatement("insert into user values(?,?,?)");
 			while(true){
+					System.out.print("enter name");
+				String name=sc.next();
+				System.out.print("enter email");
+				String email=sc.next();
+				System.out.print("enter pass");
+				String pass=sc.next();
+				stmt.setString(1, name);
+				stmt.setString(2, email);
+				stmt.setString(3, pass);
+				stmt.executeUpdate();
