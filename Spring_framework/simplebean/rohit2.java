d0e5d79b419b66bc8ee7fa1cce5eb9f9099b0d5d
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2016-07-16 12:41:43 +0530

    Update rohit2.java

--- Spring_framework/simplebean/rohit2.java
+++ Spring_framework/simplebean/rohit2.java
@@ -1,1 +1,17 @@
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class rohit2 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		ApplicationContext context=new ClassPathXmlApplicationContext("bean.xml");
+		rohit1 obj=(rohit1)context.getBean("aa");
+		obj.getA();
+
+	}
+
+}
