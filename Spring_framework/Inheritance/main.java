bba62b27d99617b5a936f7158b0bac4424f8648b
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2016-07-17 23:09:15 +0530

    Update main.java

--- Spring_framework/Inheritance/main.java
+++ Spring_framework/Inheritance/main.java
@@ -1,1 +1,22 @@
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context=new ClassPathXmlApplicationContext("bean.xml");
+		parent obj1=(parent)context.getBean("1");
+		obj1.getM1();
+		obj1.getM2();
+		child boj2=(child)context.getBean("2");
+		boj2.getM1();
+		boj2.getM2();
+		boj2.getM3();
+
+	}
+
+}
+
