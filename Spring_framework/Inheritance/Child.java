ff725971a904f054832e7879288487faadaeaa9d
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2016-07-17 23:04:18 +0530

    Update Child.java

--- Spring_framework/Inheritance/Child.java
+++ Spring_framework/Inheritance/Child.java
@@ -1,1 +1,32 @@
 
+public class child {
+
+	private String m1;
+	private String m2;
+	private String m3;
+	public String getM1() {
+		System.out.println("child:"+m1);
+		return m1;
+	}
+	public void setM1(String m1) {
+		this.m1 = m1;
+		
+	}
+	public String getM2() {
+		System.out.println("child:"+m2);
+		return m2;
+	}
+	public void setM2(String m2) {
+		this.m2 = m2;
+		
+	}
+	public String getM3() {
+		System.out.println("child:"+m3);
+		return m3;
+	}
+	public void setM3(String m3) {
+		this.m3 = m3;
+		
+	}
+	
+}
