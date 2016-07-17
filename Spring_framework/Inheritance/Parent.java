706d1111d49724f297396055478c2350724f8f51
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2016-07-17 23:05:32 +0530

    Update Parent.java

--- Spring_framework/Inheritance/Parent.java
+++ Spring_framework/Inheritance/Parent.java
@@ -1,1 +1,23 @@
 
+
+public class parent {
+	private String m1;
+	private String m2;
+	public String getM1() {
+		System.out.println("parent"+m1);
+		return m1;
+	}
+	public void setM1(String m1) {
+		this.m1 = m1;
+	}
+	public String getM2() {
+		System.out.println("parent"+m2);
+		return m2;
+	}
+	public void setM2(String m2) {
+		this.m2 = m2;
+	}
+	
+	
+
+}
