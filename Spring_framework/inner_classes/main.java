0cc4d3b1ade7d87a0e050cbedeaced12f90722df
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2016-07-19 14:39:09 +0530

    Update main.java

--- Spring_framework/inner_classes/main.java
+++ Spring_framework/inner_classes/main.java
@@ -1,1 +1,13 @@
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class main {
+	public static void main() {
+		ApplicationContext context=new ClassPathXmlApplicationContext("beam.xml");
+		second obj=(second)context.getBean("xyz");
+		
+	}
+
+}
